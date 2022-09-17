bc7c8292e2cb749744e7e04d547186281b4bc004
sombriks <sombriks@gmail.com>
2022-09-17 14:10:54 -0300

    teste secrets do repositório

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="helloStore.increment">Counter: {{ helloStore.count }}</button>
+    <p>Doubled is {{helloStore.doubleCount}}</p>
   </div>
 </template>
+
+<script setup>
+import {useCounterStore} from "../stores/counter"
+
+const helloStore = useCounterStore()
+
+</script>
 
 <style>
 @media (min-width: 1024px) {
@@ -10,6 +19,9 @@
     min-height: 100vh;
     display: flex;
     align-items: center;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
   }
 }
 </style>
