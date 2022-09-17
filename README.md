6496f299237a6d2b7c08df07c09cf33cf98f2cf6
sombriks <sombriks@gmail.com>
2022-09-17 01:00:32 -0300

    testando build na nuvem

--- README.md
+++ README.md
@@ -14,7 +14,17 @@ npm firebase init
 
 select any feature you wish
 
-add `firebase emulators:start` on package.json as the "dev" script, it's cool
+add `firebase emulators:start` on package.json as the "emulators" script, it's
+cool
+
+create another project with [create-vue](https://github.com/vuejs/create-vue)
+and move the artifacts into this new project.
+
+change `hosting>public` folder from _public_ to _dist_ in
+[firebase.json](firebase.json).
+
+create an application on firebase web console and put the info inside a .env
+file
 
 ## next steps
 
--- src/main.js
+++ src/main.js
@@ -2,19 +2,28 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 
 import { initializeApp } from "firebase/app";
+import { getFunctions, httpsCallable } from 'firebase/functions';
 
 import App from "./App.vue";
 import router from "./router";
 
 import "./assets/main.css";
 
-const fb = initializeApp({ projectId: "sample-firebase-2022 " });
-window.fb = fb
-// fb.functions()
-//   .httpsCallable("helloWorld")()
-//   .then((ret) => {
-//     console.log(ret);
-//   });
+const fb = initializeApp({
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+});
+
+const functions = getFunctions(fb);
+
+httpsCallable(functions, "helloWorld")().then((ret) => {
+  console.log(ret);
+});
 
 const app = createApp(App);
 
