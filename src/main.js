e714ebd5b071936b8e8cb1a8836af84225a4ad41
sombriks <sombriks@gmail.com>
2022-09-16 23:45:02 -0300

    teste

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
+import { initializeApp } from "firebase/app";
+
 import App from "./App.vue";
 import router from "./router";
 
 import "./assets/main.css";
+
+const fb = initializeApp({ projectId: "sample-firebase-2022 " });
+window.fb = fb
+// fb.functions()
+//   .httpsCallable("helloWorld")()
+//   .then((ret) => {
+//     console.log(ret);
+//   });
 
 const app = createApp(App);
 
