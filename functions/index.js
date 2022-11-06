bda1ec56ae9273e5a1503f0ee85b7fac1db94479
sombriks <sombriks@gmail.com>
2022-11-06 14:36:30 -0300

    fix cors on cloud functions

--- functions/index.js
+++ functions/index.js
@@ -6,8 +6,8 @@ const functions = require("firebase-functions");
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.set("Access-Control-Allow-Origin", "*");
-  response.set("Access-Control-Allow-Headers", "Content-Type");
-  functions.logger.info("Hello logs!", { structuredData: true });
+  response.set("Access-Control-Allow-Headers", ["Content-Type", "Authorization"]);
+  functions.logger.info("Hello logs!", {structuredData: true});
   response.send({
     data: ["Hello from Firebase!", new Date()],
   });
