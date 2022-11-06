08f5fc3a50e7d56cba3bc3a9ca63a6d1541ca404
sombriks <sombriks@gmail.com>
2022-11-06 14:14:33 -0300

    sampling authentication

--- src/views/AuthenticateView.vue
+++ src/views/AuthenticateView.vue
@@ -2,19 +2,27 @@
   <div>
     <h1>Authenticate</h1>
     <button @click="fbLogin">Login</button>
+    <p v-if="user.email">
+      Hello, {{ user.displayName }}!
+    </p>
+    <p v-else>
+      {{ user.displayName }}
+    </p>
   </div>
 </template>
 
 <script setup>
 import {ref} from "vue"
 import {fbAuth} from "../config/firebase"
-import {GoogleAuthProvider, signInWithPopup } from "firebase/auth"
+import {GoogleAuthProvider, signInWithPopup} from "firebase/auth"
 
+const user = ref({displayName: "Not logged"})
 const fbLogin = (e) => {
   console.log(e)
   const provider = new GoogleAuthProvider();
   signInWithPopup(fbAuth, provider).then(result => {
     console.log(result)
+    user.value = result.user
   })
 }
 </script>
